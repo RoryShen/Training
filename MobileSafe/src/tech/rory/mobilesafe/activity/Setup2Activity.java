c7eb2bdd6cb1cbd97d99d55e742187814b6c7a73
Rory <roryshen@gmail.com>
2017-08-05 00:08:24 +0800

    Add oop page.

--- MobileSafe/src/tech/rory/mobilesafe/activity/Setup2Activity.java
+++ MobileSafe/src/tech/rory/mobilesafe/activity/Setup2Activity.java
@@ -2,20 +2,21 @@ package tech.rory.mobilesafe.activity;
 
 import tech.rory.mobilesafe.R;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Setup1Activity extends Activity {
+public class Setup2Activity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_setup1);
+		setContentView(R.layout.activity_setup2);
 	}
 
 	public void next(View view) {
-		// 跳转到下一页面
-		startActivity(new Intent(this, Setup2Activity.class));
-		finish();
+
+	}
+
+	public void previous(View view) {
+
 	}
 }
