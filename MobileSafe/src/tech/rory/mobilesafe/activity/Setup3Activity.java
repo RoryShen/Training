a31552426a01ee82ee56e93df96a6738a44d3bf9
chao.shen <chao.shen@ck-telecom.com>
2017-08-10 18:16:38 +0800

    finish contacts select

--- MobileSafe/src/tech/rory/mobilesafe/activity/Setup3Activity.java
+++ MobileSafe/src/tech/rory/mobilesafe/activity/Setup3Activity.java
@@ -4,13 +4,18 @@ import tech.rory.mobilesafe.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.view.View;
+import android.widget.EditText;
 
 public class Setup3Activity extends BaseSetupActivity {
+	private EditText etphoneEditText;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setup3);
+		etphoneEditText = (EditText) findViewById(R.id.et_phone);
 	}
 
 	@Override
@@ -28,5 +33,20 @@ public class Setup3Activity extends BaseSetupActivity {
 		finish();
 		overridePendingTransition(R.anim.prevtran_in, R.anim.prevtran_out);
 
+	}
+
+	public void SelectContacts(View view) {
+		// startActivity(new Intent(this, ContactsActivity.class));
+		startActivityForResult(new Intent(this, ContactsActivity.class), 0);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		String phoneString = data.getStringExtra("phone");
+		phoneString.replaceAll("-","").replaceAll(" ", "");
+		etphoneEditText.setText(phoneString);
 	}
 }
