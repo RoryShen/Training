d125e8f14c116d2f5c87b0319db0ab28967c76dd
RoryShen <roryshen@gmail.com>
2017-09-16 14:11:49 +0800

    Text watcher

--- MobileSafe/src/tech/rory/mobilesafe/activity/AddressActivity.java
+++ MobileSafe/src/tech/rory/mobilesafe/activity/AddressActivity.java
@@ -5,7 +5,9 @@ import tech.rory.mobilesafe.db.dao.AddressDao;
 import tech.rory.mobilesafe.utils.ToastUtils;
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -26,9 +28,39 @@ public class AddressActivity extends Activity {
 		setContentView(R.layout.activity_adress);
 		etNumber = (EditText) findViewById(R.id.et_phone);
 		tvResult = (TextView) findViewById(R.id.tv_result);
+		
+		//给文本设置监听器
+		etNumber.addTextChangedListener(new TextWatcher() {
+			
+
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before,
+					int count) {
+				addressQuery();
+
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				addressQuery();
+
+			}
+		});
 	}
 
 	public void query(View view) {
+		addressQuery();
+
+	}
+	private void addressQuery(){
 		String number = etNumber.getText().toString().trim();
 		if (!TextUtils.isEmpty(number)) {
 			String address = AddressDao.getAddress(number);
@@ -36,6 +68,6 @@ public class AddressActivity extends Activity {
 			// ToastUtils.showToast(this, number);
 
 		}
-
+		
 	}
 }
